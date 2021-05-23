1d48754115ee4d1e3ae5ea9ec02d9925675ed949
EvgenySushko <evgeny.sushko@gmail.com>
2021-05-23 09:43:23 +0100

    blog posts creating and displaying added

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import EditProfileForm, LoginForm, RegisterForm
-from app.models import User
+from app.forms import EditProfileForm, LoginForm, PostForm, RegisterForm
+from app.models import User, Post
 
 
 @app.before_request
@@ -14,15 +14,26 @@ def before_request():
         db.session.commit()
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
-    return render_template("index.html", title="Home", posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for("index"))
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title="Home", form=form, posts=posts)
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template("index.html", title="Explore", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -68,10 +79,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"},
-    ]
+    posts = Post.query.filter_by(user_id=current_user.id).all()
     return render_template("user.html", user=user, posts=posts)
 
 
